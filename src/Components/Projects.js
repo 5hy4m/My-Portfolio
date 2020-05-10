ac034c819f539ce3e98c5481c9ebc2fd462244bb
ShyamKumar <vcsshyam1998@gmail.com>
2020-05-10 18:50:02 +0530

    Add blog project to projects.js Component

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,37 +1,61 @@
 import React from 'react';
 
 const Projects = () => {
-    return (
-        <section
-            className="resume-section p-3 p-lg-5 d-flex justify-content-center"
-            id="projects">
-            <div className="w-100">
-                <h2 className="mb-5">Projects</h2>
-                <div
-                    className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-                    <div className="resume-content">
-                        <h3 className="mb-0">Simple ChatRoom - React & Django</h3>
-                        <div className="subheading mb-3"><a href="https://github.com/5hy4m/ChatRoom "><i className="fab fa-github" />   Github Link</a> </div>
-                    </div>
-                    <div className="resume-date text-md-right">
-                    </div>
-                </div>
-                <div
-                    className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-                    <div className="resume-content">
-                        <h3 className="mb-0">An Inventory Management System Still under development</h3>
-                        <div className="subheading">Django REST Api</div>
-                        <div className="subheading mb-3"><a href="https://github.com/5hy4m/ChatRoom "><i className="fab fa-github" />   Github Link</a> </div>
-                            <div className="subheading">Frontend With React</div>
-                            <div className="subheading mb-3"><a href="https://github.com/5hy4m/ChatRoom "><i className="fab fa-github" />   Github Link</a> </div>
-                    </div>
-                    <div className="resume-date text-md-right">
-                    </div>
-                </div>
-                
+  return (
+    <section
+      className="resume-section p-3 p-lg-5 d-flex justify-content-center"
+      id="projects">
+      <div className="w-100">
+        <h2 className="mb-5">Projects</h2>
+        <div
+          className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="resume-content">
+            <h3 className="mb-0">Simple ChatRoom - React & Django</h3>
+            <div className="subheading mb-3">
+              <a target="_blank" href="https://github.com/5hy4m/ChatRoom "><i className="fab fa-github"/>
+                Github Link</a>
             </div>
-        </section>
-    );
+          </div>
+          <div className="resume-date text-md-right"></div>
+        </div>
+        <div
+          className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="resume-content">
+            <h3 className="mb-0">Blog - React & Django api</h3>
+            <div className="subheading">Django REST Api</div>
+            <div className="subheading mb-3">
+              <a target="_blank" href="https://github.com/5hy4m/SimpleBolg-api  "><i className="fab fa-github"/>
+                Github Link</a>
+            </div>
+            <div className="subheading">Frontend With React</div>
+            <div className="subheading mb-3">
+              <a target="_blank" href="https://github.com/5hy4m/Simple-Blog "><i className="fab fa-github"/>
+                Github Link</a>
+            </div>
+          </div>
+          <div className="resume-date text-md-right"></div>
+        </div>
+        <div
+          className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="resume-content">
+            <h3 className="mb-0">An Inventory Management System Still under development</h3>
+            <div className="subheading">Django REST Api</div>
+            <div className="subheading mb-3">
+              <a target="_blank" href="https://github.com/5hy4m/ChatRoom "><i className="fab fa-github"/>
+                Github Link</a>
+            </div>
+            <div className="subheading">Frontend With React</div>
+            <div className="subheading mb-3">
+              <a target="_blank" href="https://github.com/5hy4m/ChatRoom "><i className="fab fa-github"/>
+                Github Link</a>
+            </div>
+          </div>
+          <div className="resume-date text-md-right"></div>
+        </div>
+
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
