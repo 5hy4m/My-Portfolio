99a33f70acfb67a82cce215a8c0e793e1042bc9e
5hy4m <vcsshyam1998@gmail.com>
2023-01-03 19:18:18 +0530

    Added google authentication to the app

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -1,45 +1,35 @@
-import React, {useState,useEffect} from 'react'
-import {msgsRef} from '../firebase'
+import React from 'react';
 
-function Admin(props) {
-    const [messages,
-        setmessages] = useState('');
-    useEffect(() => {
-        msgsRef.on('value', snapshot => {
-            let data = []
-            const db = snapshot.val()
-            Object.keys(db).forEach(element => {
-                    data.push(db[element])
-                });
-            setmessages(data)
-        });
-    },[props]);
-    console.log(messages);
-    
-    const msgs = messages === '' ? <div>
-        LODAING
-    </div> : messages.map((x,index)=><div
-        key = {index}
-        className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-        <div className="resume-content">
-<h3 className="mb-0">{x.name}</h3>
-<div className="subheading mb-3">Email :{x.email}    || Phone No: {x.phno}</div>
-            <p>{x.msg}</p>
-        </div>
-        <div className="resume-date text-md-right">
-<span className="text-primary">Date&Time : {x.timestamp}</span>
-        </div>
-    </div>)
-    return (
-        <section
-            className="resume-section p-3 p-lg-5 justify-content-center"
-            id="contactme">
-            <div className="contactme w-100">
-                <h2 className="mb-5">Admin</h2>
+function Admin({ messages }) {
+  return (
+    <section
+      className="resume-section p-3 p-lg-5 justify-content-center"
+      id="contactme"
+    >
+      <div className="contactme w-100">
+        <h2 className="mb-5">Admin</h2>
+      </div>
+      {messages.map((x, index) => (
+        <div
+          key={index}
+          className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5"
+        >
+          {' '}
+          <div className="resume-content">
+            <h3 className="mb-0">{x.name}</h3>
+            <div className="subheading mb-3">
+              Email :{x.email}
+              || Phone No: {x.phno}
             </div>
-            {msgs}
-        </section>
-    )
+            <p>{x.msg}</p>
+          </div>
+          <div className="resume-date text-md-right">
+            <span className="text-primary">Date&Time : {x.timestamp}</span>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
 }
 
-export default Admin
+export default Admin;
--- src/firebase.js
+++ src/firebase.js
@@ -1,18 +1,26 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyBOcAgoDitRxLlhbdXv37vt-f_iW1s9S9I",
-  authDomain: "my-portfolio-api-db.firebaseapp.com",
-  databaseURL: "https://my-portfolio-api-db.firebaseio.com",
-  projectId: "my-portfolio-api-db",
-  storageBucket: "my-portfolio-api-db.appspot.com",
-  messagingSenderId: "849641430417",
-  appId: "1:849641430417:web:ed226f5f4616eaa9d3ce30",
-  measurementId: "G-WM6MSCPKX6"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_Id,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  auth: process.env.REACT_APP_UID,
 };
-  
+const provider = new firebase.auth.GoogleAuthProvider();
+const app = firebase.initializeApp(config);
+export const adminApp = async () =>
+  await new Promise((resolve) =>
+    app
+      .auth()
+      .signInWithPopup(provider)
+      .then(() => resolve(app.database().ref().child('Messages')))
+  );
 
-firebase.initializeApp(config);
 const databaseRef = firebase.database().ref();
-export const msgsRef = databaseRef.child("Messages")
-export const credRef = databaseRef.child("Credentials")
+
+export const msgsRef = databaseRef.child('Messages');
